6558492ff33e7234ad26ff3975fa5e9867eca9ee
Sciamenna <maronno@intrage.it>
2013-07-25 11:52:02 +0200

    SecutiryService allineato all'EntityManagerFactory globale.

--- src/it/univaq/mwt/j2ee/kmZero/business/impl/JPASecurityService.java
+++ src/it/univaq/mwt/j2ee/kmZero/business/impl/JPASecurityService.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
 import javax.persistence.Query;
 
 import org.springframework.stereotype.Service;
@@ -18,13 +19,15 @@ import it.univaq.mwt.j2ee.kmZero.business.SecurityService;
 import it.univaq.mwt.j2ee.kmZero.business.model.Role;
 import it.univaq.mwt.j2ee.kmZero.business.model.User;
 
-@Service
+
 public class JPASecurityService implements SecurityService {
 
+	@PersistenceUnit
+	private EntityManagerFactory emf;
+	
 	@Override
 	public User authenticate(String e) throws BusinessException {
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("kmz");
         EntityManager em = emf.createEntityManager();
    
         User result = null;
@@ -34,7 +37,6 @@ public class JPASecurityService implements SecurityService {
         result = (User)query.getSingleResult();
         System.out.println("USER:"+result.getName());
         em.close();
-        emf.close();
        
         return result;
 	}
